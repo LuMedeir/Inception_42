37a28bbd079302fad10dae330a7ad7187bd12761
Luiza Medeiros <126430433+LuMedeir@users.noreply.github.com>
2024-06-20 12:01:48 -0300

    Update script.sh

--- srcs/requirements/wordpress/tool/script.sh
+++ srcs/requirements/wordpress/tool/script.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Esse script automatiza a configuração inicial do WordPress dentro 
+# de um contêiner Docker, instalar o WordPress, configurar o banco de 
+# dados e aguardar até que o banco de dados esteja pronto antes de iniciar 
+# o PHP-FPM para servir o site WordPress.
+
+
+# Configura o arquivo wp-config.php com as credenciais do banco de dados.
 wp --allow-root config create \
 	--path=/var/www/html \
 	--dbname="$WORDPRESS_DATABASE" \
@@ -8,6 +15,7 @@ wp --allow-root config create \
 	--dbhost=mariadb \
 	--dbprefix="wp_"
 
+# Cria o usuário administrador do WordPress e configura outras opções básicas, como título do site e URL.
 wp core install --allow-root \
 	--path=/var/www/html \
 	--title="Inception" \
@@ -21,4 +29,6 @@ while ! wp db check --allow-root --path=/var/www/html/; do
 	sleep 1
 done
 
+
+# O comando exec é utilizado no shell script para substituir o processo atual pelo processo especificado. 
 exec php-fpm7.4 -F
