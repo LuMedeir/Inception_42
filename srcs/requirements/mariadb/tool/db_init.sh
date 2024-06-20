e6a5fae8ab6215514dd1395620dedaaa09ac09fc
Luiza Medeiros <126430433+LuMedeir@users.noreply.github.com>
2024-06-20 12:33:22 -0300

    Documentation db_init.sh

--- srcs/requirements/mariadb/tool/db_init.sh
+++ srcs/requirements/mariadb/tool/db_init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Esse script automatiza a configuração inicial do MariaDB para um ambiente onde 
+# o WordPress será configurado posteriormente. Ele inicia o serviço do MariaDB, 
+# cria um banco de dados, um usuário específico para o WordPress, concede todos 
+# os privilégios necessários ao usuário sobre o banco de dados e garante que as 
+# alterações sejam aplicadas imediatamente.
+
 service mariadb start
 
 mariadb -u root -e \
